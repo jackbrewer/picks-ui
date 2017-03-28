c29ff93200e4c45c8dd3db384785cd21958cee99
Jack Brewer <jack@jackbrewer.co.uk>
2017-03-28 22:20:16 +0100

    First steps towards PWA

--- src/index.js
+++ src/index.js
@@ -4,3 +4,7 @@ import App from './layout/app/App'
 import './asset/css/index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))
+
+if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
+  window.addEventListener('load', () => navigator.serviceWorker.register('/service-worker.js'))
+}
