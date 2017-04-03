1e2e00e7002e9df785ee98ad7ed0f839d6bf1f33
Jack Brewer <jack@jackbrewer.co.uk>
2017-04-03 13:08:02 +0100

    Adds react-snapshot, moves deploy from gh-pages to surge

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { render } from 'react-snapshot'
 import App from './layout/app/App'
 import './asset/css/index.css'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+render(<App />, document.getElementById('root'))
 
 if ('serviceWorker' in navigator && process.env.NODE_ENV === 'production') {
   window.addEventListener('load', () => navigator.serviceWorker.register('/service-worker.js'))
--- src/layout/app/App.js
+++ src/layout/app/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { HashRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Helmet from 'react-helmet'
 
 // Layout Components
